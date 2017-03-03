d1877f71aea49322ea56cf5141a1620d62daa8b6
pavelmacu <pavel.alfredo.macutela@estudiant.upc.edu>
2017-03-03 11:47:33 +0100

    client.py

---
+++ client.py
@@ -0,0 +1,11 @@
+import socket
+
+HOST = '10.192.107.42'    # The remote host
+PORT = 5000              # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall('Hello, world')
+data = s.recv(1024)
+s.close()
+print 'Received', repr(data)
+
