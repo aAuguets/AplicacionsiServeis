388f9b5850e2303ca0522e50b74277fa74545ea7
Adria Auguets <adriiauguets@gmail.com>
2017-03-08 13:14:51 +0100

    pujat

--- P2/UDPxat.py
+++ P2/UDPxat.py
@@ -6,45 +6,34 @@
 import socket, select, sys, signal, struct
 
 S_HOST = ''
-C_HOST = '10.192.107.42' #adreça de la maquina que controlara els msg que li arriben.
+C_HOST = '127.0.0.1' #adreça de la maquina que controlara els msg que li arriben.
 PORT = 5000 
-addr = ''
 
 
 def s_exit(signal, frame):
 	s.close()
 	sys.exit(0)
 
-def _select(data, ip):
+def _select(data):
 	read, _, _ = select.select(data, [], [])
 	if read[0] == data[0]:
 		txt = sys.stdin.readline()
-		data[1].sendto(txt, (ip, PORT))
+		data[1].sendto(txt, (C_HOST, PORT))
 	elif read[0] == data[1]:
 		txt, addr = data[1].recvfrom(1024)
 		print addr, "->", txt
-
-def _select2(data, ip):
-	read, _, _ = select.select(data, [], [])
-	addr = ip
-	if read[0] == data[0]:
-		txt = sys.stdin.readline()
-		data[1].sendto(txt, (ip, PORT))
-	elif read[0] == data[1]:
-		txt, addr = data[1].recvfrom(1024)
-		print addr, "->", txt
-
 
 
 def main():
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.bind((S_HOST, PORT))
-	signal.signal(signal.SIGINT, s_exit)
+	if sys.argv[1]=='0':
+		s.bind((S_HOST, PORT))
+		signal.signal(signal.SIGINT, s_exit)
+		print "S"
 
-	if (sys.argv[1]==0):
-		while 1:
-			_select([sys.stdin, s],C_HOST)
-	else:
-		while 1:
-			_select([sys.stdin, s],C_HOST)
+	while 1:
+		_select([sys.stdin, s])
+
+
+
 main()
